0aff873d3eb7f7903bad17144de2cfe04bebf4db
Roberto Robles <rob2thinking@gmail.com>
2021-08-30 14:36:27 -0400

    Create test_validator.py

---
+++ test_validator.py
@@ -0,0 +1,56 @@
+#####################
+# Imports           #
+#####################
+
+import datetime
+import unittest
+from validator import Validator
+
+#####################
+# Classes           #
+#####################
+
+class TestValidator(unittest.TestCase):
+
+    def test_check_int(self):
+        '''checks if the field is an integer'''
+
+        # Assume
+        field = "daily_limit"
+        value = 10
+        validator = Validator()
+
+        # Action
+        result = validator.check_int(field, value)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_check_float(self):
+        '''checks if the field is a float'''
+
+        # Assume
+        field = "percent_change"
+        value = 10.0
+        validator = Validator()
+
+        # Action
+        result = validator.check_float(field, value)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_check_string(self):
+        '''checks if the field is a string'''
+
+        # Assume
+        field = "symbol"
+        value = "AAPL"
+        validator = Validator()
+
+        # Action
+        result = validator.check_string(field, value, 10)
+
+        # Assert
+        self.assertTrue(result)
+        
